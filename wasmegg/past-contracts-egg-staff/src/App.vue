5dcfffabafde58acda63824209a8e062027bda7a
Dane Stapleton <danestaples13@gmail.com>
2022-10-27 16:50:58 -0500

    remove reference to legendary study

--- wasmegg/past-contracts-egg-staff/src/App.vue
+++ wasmegg/past-contracts-egg-staff/src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <the-nav-bar active-entry-id="rockets-tracker" />
+  <the-nav-bar active-entry-id="past-contracts" />
 
   <h1 class="mx-4 mt-4 mb-2 text-center text-lg leading-6 font-medium text-gray-900">
-    Rockets tracker
+    Past contracts viewer
   </h1>
 
-  <div class="max-w-5xl w-full pb-6 mx-auto">
-    <the-player-id-form :player-id="playerId" @submit="submitPlayerId" />
+  <the-player-id-form :player-id="playerId" @submit="submitPlayerId" />
 
-    <legendaries-study-opt-in-form :event-bus="eventBus" class="mx-4 xl:mx-0 my-4" />
-
+  <div class="max-w-5xl w-full px-4 pb-4 xl:px-0 mx-auto">
     <!-- Use a key to recreate on data loading -->
     <base-error-boundary v-if="playerId" :key="`${playerId}:${refreshId}`">
       <suspense>
         <template #default>
-          <the-report :player-id="playerId" :event-bus="eventBus" />
+          <the-report :player-id="playerId" />
         </template>
         <template #fallback>
           <base-loading />
@@ -23,9 +21,9 @@
     </base-error-boundary>
 
     <template v-else>
-      <div class="text-sm mt-4 mx-4 xl:mx-0">
-        This tool presents the player's current rocket mission statuses, aggregate historical
-        mission statistics, launch log, artifact collection progress, and more.
+      <div class="text-sm">
+        This tool pulls the current save of the specified player, and generates a report with their
+        Eggs of Prophecy (PE) progress and completion statuses of all known contracts.
       </div>
     </template>
   </div>
@@ -33,14 +31,12 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import mitt from 'mitt';
 
 import { getSavedPlayerID, savePlayerID } from 'lib';
-
 import BaseErrorBoundary from 'ui/components/BaseErrorBoundary.vue';
 import BaseLoading from 'ui/components/BaseLoading.vue';
-import TheNavBar from 'ui/components/NavBar.vue';
 import ThePlayerIdForm from 'ui/components/PlayerIdForm.vue';
+import TheNavBar from 'ui/components/NavBar.vue';
 import TheReport from '@/components/TheReport.vue';
 
 export default defineComponent({
@@ -61,11 +57,9 @@ export default defineComponent({
       refreshId.value = Date.now();
       savePlayerID(id);
     };
-    const eventBus = mitt();
     return {
       playerId,
       refreshId,
-      eventBus,
       submitPlayerId,
     };
   },
