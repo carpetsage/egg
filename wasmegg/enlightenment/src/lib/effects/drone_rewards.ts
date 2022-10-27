bdc0e524f4144dde54a05130b4b51c25fac2beb0
Dane Stapleton <danestaples13@gmail.com>
2022-10-27 16:56:07 -0500

    remove tools Im not forking

--- wasmegg/enlightenment/src/lib/effects/drone_rewards.ts
+++
@@ -1,7 +0,0 @@
-import { ei } from 'lib';
-import { Artifact } from '../types';
-import { multiplicativeEffect } from './common';
-
-export function droneRewardsMultiplier(artifacts: Artifact[]): number {
-  return multiplicativeEffect(artifacts, [ei.ArtifactSpec.Name.AURELIAN_BROOCH]);
-}
