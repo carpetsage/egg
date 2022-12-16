61f63a77528c88d548156b18f5deddf64a152640
carpetsage <carpetsage@protonmail.com>
2022-12-15 20:40:21 -0600

    udpate version and protos for 1.25.4

--- lib/api/version.ts
+++ lib/api/version.ts
@@ -1,7 +1,7 @@
 import { ei } from '../proto';
 
-export const APP_VERSION = '1.25.1';
-export const APP_BUILD = '1.25.1.0';
-export const CLIENT_VERSION = 43;
+export const APP_VERSION = '1.25.4';
+export const APP_BUILD = '111225';
+export const CLIENT_VERSION = 45;
 export const PLATFORM_STRING = 'IOS';
 export const PLATFORM = ei.Platform.IOS;
