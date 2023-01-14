726c7a6579dc921ea6df2a1f7ab269bb1b96556b
Carpet Sage <carpetsage@protonmail.com>
2023-01-14 16:01:50 -0600

    also delete isNew field to simplify diffs

--- update-egg-config-gist/update-config.sh
+++ update-egg-config-gist/update-config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ./getConfig.py > /tmp/config.json
-jq -S 'walk(if type == "object" and has("secondsRemaining") then del(.secondsRemaining) else . end)' /tmp/config.json > config.json
+# delete isNew and secondsRemaining fields if they exist
+jq -S 'walk(try (del(.isNew) | del(.secondsRemaining))//.)' /tmp/config.json > config.json
 wget https://gist.githubusercontent.com/carpetsage/${GIST_ID}/raw/config.json -O /tmp/gist.json
 # push gist if there's a changes
 diff config.json /tmp/gist.json || node ./push-gist.js
