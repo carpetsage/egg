6d25952ff39b1d0e1cd88d0da778282fec5c0c82
Carpet Sage <carpetsage@protonmail.com>
2023-01-15 19:57:35 -0600

    trim some stuff down

--- wasmegg/eggs-laid/src/components/TheReport.vue
+++ wasmegg/eggs-laid/src/components/TheReport.vue
@@ -1,72 +1,17 @@
 <template>
   <player-card :backup="backup" :inventory="inventory" />
-
-  <collapsible-section
-    section-title="Active missions"
-    :visible="isVisibleSection('active-missions')"
-    @toggle="toggleSectionVisibility('active-missions')"
-  >
-    <active-missions-report :active-missions="activeMissions" :backup="backup" />
-  </collapsible-section>
-
-  <collapsible-section
-    section-title="Mission statistics"
-    :visible="isVisibleSection('mission-statistics')"
-    @toggle="toggleSectionVisibility('mission-statistics')"
-  >
-    <mission-statistics-report :artifacts-d-b="artifactsDB" :progress="progress" />
-  </collapsible-section>
-
-  <collapsible-section
-    section-title="Launch log"
-    :visible="isVisibleSection('launch-log')"
-    @toggle="toggleSectionVisibility('launch-log')"
-  >
-    <launch-log :user-id="playerId" :artifacts-d-b="artifactsDB" />
-  </collapsible-section>
-
-  <collapsible-section
-    section-title="Artifact loadouts"
-    :visible="isVisibleSection('artifact-loadouts')"
-    @toggle="toggleSectionVisibility('artifact-loadouts')"
-  >
-    <artifact-loadouts-report :backup="backup" />
-  </collapsible-section>
-
-  <collapsible-section
-    section-title="Artifacting progress"
-    :visible="isVisibleSection('artifacting-progress')"
-    @toggle="toggleSectionVisibility('artifacting-progress')"
-  >
-    <artifacting-progress-report :player-id="playerId" :inventory="inventory" />
-  </collapsible-section>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, PropType } from 'vue';
 import { Emitter } from 'mitt';
 
-import { Inventory, requestFirstContact, UserBackupEmptyError } from 'lib';
-import { useSectionVisibility } from 'ui/composables/section_visibility';
-import { reportLegendaries } from '@/lib';
-import { REPORT_LEGENDARIES } from '@/events';
-import CollapsibleSection from '@/components/CollapsibleSection.vue';
+import {requestFirstContact, UserBackupEmptyError } from 'lib';
 import PlayerCard from '@/components/PlayerCard.vue';
-import ActiveMissionsReport from '@/components/ActiveMissionsReport.vue';
-import MissionStatisticsReport from '@/components/MissionStatisticsReport.vue';
-import LaunchLog from '@/components/LaunchLog.vue';
-import ArtifactLoadoutsReport from '@/components/ArtifactLoadoutsReport.vue';
-import ArtifactingProgressReport from '@/components/ArtifactingProgressReport.vue';
 
 export default defineComponent({
   components: {
-    CollapsibleSection,
-    PlayerCard,
-    ActiveMissionsReport,
-    MissionStatisticsReport,
-    LaunchLog,
-    ArtifactLoadoutsReport,
-    ArtifactingProgressReport,
+    PlayerCard
   },
   props: {
     playerId: {
@@ -90,26 +35,9 @@ export default defineComponent({
     if (!backup.settings) {
       throw new Error(`${playerId}: settings not found in backup`);
     }
-    const artifactsDB = backup.artifactsDb;
-    if (!artifactsDB) {
-      throw new Error(`${playerId}: no artifacts database in backup`);
-    }
-    const inventory = computed(() => new Inventory(artifactsDB));
-    const activeMissions = artifactsDB.missionInfos || [];
-    const { isVisibleSection, toggleSectionVisibility } = useSectionVisibility();
-    reportLegendaries(backup);
-    eventBus.on(REPORT_LEGENDARIES, () => {
-      console.log('event');
-      reportLegendaries(backup);
-    });
     return {
       backup,
-      progress,
-      artifactsDB,
-      inventory,
-      activeMissions,
-      isVisibleSection,
-      toggleSectionVisibility,
+      progress
     };
   },
 });
--- wasmegg/eggs-laid/src/lib/index.ts
+++ wasmegg/eggs-laid/src/lib/index.ts
@@ -1,6 +1,2 @@
-export * from './loot';
-export * from './missions';
 export * from './storage';
-export * from './study';
-export * from './study_stats'; // Generated
 export * from './types';
