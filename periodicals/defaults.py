d2b83915dc4bd7bd5efed996885a06ea0a9d3e42
Carpet Sage <carpetsage@protonmail.com>
2023-03-30 18:13:18 -0500

    use betterproto to generate readable python for events/contracts fetching

---
+++ periodicals/defaults.py
@@ -0,0 +1,21 @@
+import os 
+import ei
+
+current_client_version = 999
+client_version = 45
+version = '1.25.4'
+build = '111225'
+platform = "IOS"
+event_file = "data/events.json"
+contract_file = "data/contracts.json"
+# Get egg id from environment variable
+user_id = os.environ.get('EI_USERID')
+url = 'https://www.auxbrain.com/ei/get_periodicals'
+
+def rinfo() -> "ei.BasicRequestInfo":
+    return ei.BasicRequestInfo(
+            build          = build,
+            client_version = client_version,
+            ei_user_id     = user_id,
+            platform       = platform,
+            version        = version)
