52a903ce552723dbeeb07c0dd041eabf4f7e1a02
Carpet Sage <carpetsage@protonmail.com>
2023-04-11 11:38:39 -0500

    one more link fix

--- wasmegg/past-contracts-eicoop2/vite.config.ts
+++ wasmegg/past-contracts-eicoop2/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/past-contracts-eicowop/',
+  base: '/past-contracts-eicoop2/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
