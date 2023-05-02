8e9995bb4417b9c35c498db27ee9963f84d24360
Carpet Sage <carpetsage@protonmail.com>
2023-05-01 19:59:45 -0500

    fix eggs-laid

--- wasmegg/eggs-laid/src/contracts.ts
+++ wasmegg/eggs-laid/src/contracts.ts
@@ -1,5 +1,5 @@
-import { ei, decodeMessage } from 'lib';
-import contractProtos from './contracts.json';
+import { decodeMessage, ei } from "lib";
+import contractProtos from "./contracts.json";
 
 export enum ContractLeague {
   Elite = 0,
@@ -17,6 +17,7 @@ export interface UserContract {
   contribution: number;
   isCoop: boolean;
   coopCode: string | null;
+  hasGrades: boolean;
   hasLeagues: boolean;
   attempted: boolean;
   league: ContractLeague;
@@ -35,9 +36,9 @@ const LEGGACY_CONTRACT_VALID_DURATION = 7 * 86400;
 
 export const rawContractList: ContractProps[] = (() => {
   const list: ContractProps[] = contractProtos
-    .map(c => decodeMessage(ei.Contract, c.proto) as ei.IContract)
+    .map((c) => decodeMessage(ei.Contract, c.proto) as ei.IContract)
     .sort((c1, c2) => c1.expirationTime! - c2.expirationTime!)
-    .map(c => ({
+    .map((c) => ({
       ...c,
       offeringTimestamp: 0,
     }));
@@ -45,11 +46,13 @@ export const rawContractList: ContractProps[] = (() => {
   for (const contract of list) {
     if (count.has(contract.identifier!)) {
       // Leggacy
-      contract.offeringTimestamp = contract.expirationTime! - LEGGACY_CONTRACT_VALID_DURATION;
+      contract.offeringTimestamp = contract.expirationTime! -
+        LEGGACY_CONTRACT_VALID_DURATION;
       count.set(contract.identifier!, count.get(contract.identifier!)! + 1);
     } else {
       // Original
-      contract.offeringTimestamp = contract.expirationTime! - ORIGINAL_CONTRACT_VALID_DURATION;
+      contract.offeringTimestamp = contract.expirationTime! -
+        ORIGINAL_CONTRACT_VALID_DURATION;
       count.set(contract.identifier!, 1);
     }
   }
@@ -57,14 +60,15 @@ export const rawContractList: ContractProps[] = (() => {
 })();
 
 export function getUserContractList(backup: ei.IBackup): UserContract[] {
-  const localContracts: ei.ILocalContract[] = (backup.contracts?.contracts || []).concat(
-    backup.contracts?.archive || []
-  );
+  const localContracts: ei.ILocalContract[] =
+    (backup.contracts?.contracts || []).concat(
+      backup.contracts?.archive || [],
+    );
   const attemptedContracts = localContracts
-    .map(c => newUserContract(c, true))
+    .map((c) => newUserContract(c, true))
     .sort((c1, c2) => c1.timestamp - c2.timestamp);
 
-  const contractIds = new Set(attemptedContracts.map(c => c.id));
+  const contractIds = new Set(attemptedContracts.map((c) => c.id));
   const unattemptedContracts = [];
   // Go through the full contract list reverse chronologically so that we only
   // pick up latest incarnations of unattempted contracts.
@@ -81,7 +85,7 @@ export function getUserContractList(backup: ei.IBackup): UserContract[] {
 function newUserContract(
   contract: ei.ILocalContract,
   attempted: boolean,
-  offeringTimestamp?: number
+  offeringTimestamp?: number,
 ): UserContract {
   const timestamp = attempted ? contract.timeAccepted! : offeringTimestamp;
   if (!timestamp) {
@@ -95,12 +99,18 @@ function newUserContract(
   const name = props.name!;
   const egg = props.egg!;
   const isCoop = !!props.maxCoopSize && props.maxCoopSize > 1;
-  const contribution = (isCoop ? contract.coopLastUploadedContribution : contract.lastAmountWhenRewardGiven) || 0;
+  const contribution =
+    (isCoop
+      ? contract.coopLastUploadedContribution
+      : contract.lastAmountWhenRewardGiven) || 0;
   const coopCode = contract.coopIdentifier || null;
   const league: ContractLeague = contract.league || 0;
   let hasLeagues = false;
-  let goals = props.goals;
-  if (props.goalSets && props.goalSets.length > league) {
+  const hasGrades = Boolean(contract.grade);
+  let goals = contract.grade
+    ? props.gradeSpecs![contract.grade - 1].goals
+    : props.goals;
+  if (!contract.grade && props.goalSets && props.goalSets.length > league) {
     hasLeagues = true;
     goals = props.goalSets[league].goals;
   }
@@ -130,6 +140,7 @@ function newUserContract(
     contribution,
     isCoop,
     coopCode,
+    hasGrades,
     hasLeagues,
     attempted,
     league,
@@ -150,6 +161,6 @@ function newUnattemptedUserContract(props: ContractProps): UserContract {
       contract: props,
     },
     false,
-    props.offeringTimestamp
+    props.offeringTimestamp,
   );
 }
