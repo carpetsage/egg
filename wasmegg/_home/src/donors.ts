3bed72d799d50423a178a9bf1e194c1a5b3d10f1
Carpet Sage <carpetsage@protonmail.com>
2023-05-05 22:34:02 -0500

    update donors

--- wasmegg/_home/src/donors.ts
+++ wasmegg/_home/src/donors.ts
@@ -1,6 +1,9 @@
 type FixedLengthArray<T, L extends number> = [T, ...T[]] & { length: L };
 
-export const donors: FixedLengthArray<string, 2> = [
+export const donors: FixedLengthArray<string, 5> = [
   "kjkawa",
   "Owen#0805",
+  "synthparadox",
+  "kok-kakarott",
+  "lorgle",
 ];
