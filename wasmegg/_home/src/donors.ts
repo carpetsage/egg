11ccc9e71bf427db90a4deca591971f3d6eacab6
Carpet Sage <carpetsage@protonmail.com>
2023-05-09 20:42:43 -0500

    udpate donor list

--- wasmegg/_home/src/donors.ts
+++ wasmegg/_home/src/donors.ts
@@ -1,9 +1,11 @@
 type FixedLengthArray<T, L extends number> = [T, ...T[]] & { length: L };
 
-export const donors: FixedLengthArray<string, 5> = [
+export const donors: FixedLengthArray<string, 7> = [
   "kjkawa",
   "Owen#0805",
   "synthparadox",
   "kok-kakarott",
   "lorgle",
+  "Menno",
+  "Vijay",
 ];
