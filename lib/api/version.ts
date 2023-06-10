ad099d4793dd3fbafe0a773305e7b21dd550a1b9
Carpet Sage <carpetsage@protonmail.com>
2023-06-09 22:17:37 -0500

    Bump versions/proto for Egg 1.27

--- lib/api/version.ts
+++ lib/api/version.ts
@@ -1,7 +1,7 @@
 import { ei } from '../proto';
 
-export const APP_VERSION = '1.26.2';
-export const APP_BUILD = '111238';
-export const CLIENT_VERSION = 48;
+export const APP_VERSION = '1.27.1';
+export const APP_BUILD = '111244';
+export const CLIENT_VERSION = 50;
 export const PLATFORM_STRING = 'IOS';
 export const PLATFORM = ei.Platform.IOS;
