422b2903db92a11f1f2e9d499fb877d2259ef3a5
Dane Stapleton <danestaples13@gmail.com>
2023-07-16 01:55:25 -0500

    version 50 -> 51

--- lib/api/version.ts
+++ lib/api/version.ts
@@ -2,6 +2,6 @@ import { ei } from '../proto';
 
 export const APP_VERSION = '1.27.3';
 export const APP_BUILD = '111247';
-export const CLIENT_VERSION = 50;
+export const CLIENT_VERSION = 51;
 export const PLATFORM_STRING = 'IOS';
 export const PLATFORM = ei.Platform.IOS;
