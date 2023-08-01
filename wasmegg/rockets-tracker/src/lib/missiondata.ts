094158ddfa1a436d535ec50ea2b3cef5a6dab5e1
carpetsage <carpetsage@protonmail.com>
2023-08-01 17:35:45 -0500

    only submit eid to menno's tool once every 24 hours

--- wasmegg/rockets-tracker/src/lib/missiondata.ts
+++ wasmegg/rockets-tracker/src/lib/missiondata.ts
@@ -1,6 +1,7 @@
 import { ei, getLocalStorage, setLocalStorage } from 'lib';
 
 const MISSIONDATA_OPT_IN_LOCALSTORAGE_KEY = 'reportMissionData';
+const LAST_REPORT_TIME_LOCALSTORAGE_KEY = 'lastMissionDataReportTime';
 const REPORT_MISSIONDATA_API_URL = 'https://eggincdatacollection.azurewebsites.net/api/SubmitEid';
 const FUNCTIONKEY = 'YdJDtLIvK1YrbC6RCO2XKm3FoIDH3pzV2jXVyBlI6sGqAzFu_SeZCQ==';
 
@@ -16,10 +17,26 @@ export function recordMissionDataPreference(optin: boolean): void {
   setLocalStorage(MISSIONDATA_OPT_IN_LOCALSTORAGE_KEY, optin);
 }
 
+export function getMissionDataSubmitTime(): number {
+  const time = getLocalStorage(LAST_REPORT_TIME_LOCALSTORAGE_KEY);
+  if (time === undefined || isNaN(Number(time))) {
+    return 0;
+  }
+  return Number(time);
+}
+export function recordMissionDataSubmitTime(): void {
+  setLocalStorage(LAST_REPORT_TIME_LOCALSTORAGE_KEY , Date.now());
+}
+
 export async function reportMissionData(backup: ei.IBackup): Promise<void> {
-  if (getLocalStorage(MISSIONDATA_OPT_IN_LOCALSTORAGE_KEY) !== 'true') {
+  const lastSubmit = getMissionDataSubmitTime();
+  console.log(Date.now() - lastSubmit);
+  // only submit if it's been 24+ hours since last time
+  if (getLocalStorage(MISSIONDATA_OPT_IN_LOCALSTORAGE_KEY) !== 'true' || Date.now() - lastSubmit < 86400000) {
+    console.log(Date.now() - lastSubmit);
     return;
   }
+
   const userId = backup.eiUserId!;
   const controller = new AbortController();
   setTimeout(() => controller.abort(), 10000);
@@ -37,7 +54,9 @@ export async function reportMissionData(backup: ei.IBackup): Promise<void> {
       }),
       signal: controller.signal,
     });
+   recordMissionDataSubmitTime();
   } catch (err) {
     console.error(err);
   }
 }
+ 
