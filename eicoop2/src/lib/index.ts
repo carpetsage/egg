6749f8b110f5d54cf9c3abf66e741865aee35e6c
Carpet Sage <carpetsage@protonmail.com>
2023-09-07 12:03:47 -0500

    remomve eicoop2

--- eicoop2/src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-export * from 'lib';
-
-export * from './contract';
-export * from './contractList';
-export * from './coop';
-export * from './features';
-export * from './solo';
-export * from './userdata';
--- eicoop2/src/symbols.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionKey, Ref } from 'vue';
-
-export const devmodeKey: InjectionKey<Ref<boolean>> = Symbol();
-export const refreshCallbackKey: InjectionKey<() => void> = Symbol();
