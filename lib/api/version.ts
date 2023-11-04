a069dfe4b0b26304065a403dee1630efe6c5406d
carpetsage <115332548+carpetsage@users.noreply.github.com>
2023-11-03 21:26:53 -0500

    update proto file (#66)
    * update proto file

--- lib/api/version.ts
+++ lib/api/version.ts
@@ -1,7 +1,7 @@
 import { ei } from '../proto';
 
-export const APP_VERSION = '1.27.5';
-export const APP_BUILD = '111249';
-export const CLIENT_VERSION = 53;
+export const APP_VERSION = '1.28.7';
+export const APP_BUILD = '111269';
+export const CLIENT_VERSION = 61;
 export const PLATFORM_STRING = 'IOS';
 export const PLATFORM = ei.Platform.IOS;
