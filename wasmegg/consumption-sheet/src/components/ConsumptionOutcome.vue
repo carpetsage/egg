af7a48ffd5356fac3624b27642197747c08ca41f
TiaBroch <tiabroch@gmail.com>
2023-11-21 16:48:50 +0100

    Cons sheet page done

--- wasmegg/consumption-sheet/src/components/ConsumptionOutcome.vue
+++ wasmegg/consumption-sheet/src/components/ConsumptionOutcome.vue
@@ -8,64 +8,55 @@
     </div>
 
     <div
-      v-if="outcome.raw_rewards.lenght == 0 || outcome.product_rewards !== null"
+      v-if="outcome.raw_rewards?.lenght == 0 || outcome.product_rewards !== null"
       class="text-xs text-lime-500"
     >
       <span
         v-tippy="{
           content:
-            'expected gold yield from fully consuming the item (recursively, down to golden eggs)',
+            'expected gold yield from fully consuming the item (recursively, down to golden eggs/piggy fill)',
         }"
         class="text-lime-500"
       >
+          <template v-if="outcome.full_consumation?.filter(f=>f.reward_type==2).length > 0||outcome.full_consumation?.filter(f=>f.reward_type==6).length > 0">
         f.c.
-        <img
-          class="h-3.5 w-3.5 inline relative -top-px -ml-0.5 -mr-1"
+          </template>
+        <template v-if="outcome.full_consumation?.filter(f=>f.reward_type==2).length > 0">
+          <img
+          class="h-3.5 w-3.5 inline relative -top-px ml-1 -mr-0.5"
           :src="iconURL('egginc-extras/icon_golden_egg.png', 64)"
         />{{ ' ' }}
-        <template v-if="outcome.full_consumation?.filter(f=>f.reward_type==2).length > 0">{{
+        {{
           formatFloat(outcome.full_consumation.filter(f=>f.reward_type==2)[0].reward_amount, { digits: 1, trim: true })
         }}</template
-        ><template v-else>0</template><template v-if="outcome.item.afx_rarity > 0">;</template>
+        >
+          <template v-if="outcome.full_consumation?.filter(f=>f.reward_type==6).length > 0">
           <img
-          class="h-3.5 w-3.5 inline relative -top-px -ml-0.5 -mr-1"
+          class="h-3.5 w-3.5 inline relative -top-px ml-1 -mr-0.5"
           :src="iconURL('egginc-extras/icon_piggy_golden_egg.png', 64)"
         />{{ ' ' }}
-        <template v-if="outcome.full_consumation?.filter(f=>f.reward_type==6).length > 0">{{
+        {{
           formatFloat(outcome.full_consumation.filter(f=>f.reward_type==6)[0].reward_amount, { digits: 1, trim: true })
         }}</template
-        ><template v-else>0</template><template v-if="outcome.item.afx_rarity > 0">;</template>
+        >
       </span>    
     </div>
   </template>
 
   <div>
     <div class="flex flex-wrap text-xs text-gray-500 leading-7 tabular-nums">
-      <span
-        v-if="outcome.raw_rewards.lenght > 0"
-        class="inline-flex items-center mr-1.5 whitespace-nowrap"
-      >
-         <div v-if="outcome.raw_rewards.filter(f=>f.reward_type==2).lenght > 0">
-        <img
-          class="h-6 w-6 -m-1 mr-0 p-1"
-          :src="iconURL('egginc-extras/icon_golden_egg.png', 64)"
-        />
-        <span v-if="outcome.product_rewards.filter(f=>f.reward_type==2) !== null" class="w-9"
-          >&times;{{ formatFloat(outcome.product_rewards.filter(f=>f.reward_type==2).reward_amount, { digits: 2 }) }}</span
-        >
-        <span v-else class="-ml-1">{{ formatFloat(outcome.product_rewards.filter(f=>f.reward_type==2).reward_amount, { digits: 2 }) }}</span>
-        </div>
-        <div v-if="outcome.raw_rewards.filter(f=>f.reward_type==6).lenght > 0">
-            <img
-              class="h-6 w-6 -m-1 mr-0 p-1"
-              :src="iconURL('egginc-extras/icon_golden_egg.png', 64)"
-            />
-            <span v-if="outcome.expected_byproducts !== null" class="w-9"
-              >&times;{{formatFloat(outcome.expected_gold, { digits: 2 }) }}</span
-            >
-            <span v-else class="-ml-1">{{ formatFloat(outcome.expected_gold, { digits: 2 }) }}</span>
-            </div>
-      </span>
+        <span v-if="outcome.raw_rewards?.filter(f=>f.reward_type==2)?.length > 0"
+              class="inline-flex items-center mr-1.5 whitespace-nowrap">
+            <img class="h-6 w-6 -m-1 mr-0 p-1"
+                 :src="iconURL('egginc-extras/icon_golden_egg.png', 64)" />
+            <span class="-ml-1">{{ formatFloat(outcome.raw_rewards?.filter(f=>f.reward_type==2)[0].reward_amount, { digits: 2 }) }}</span>
+        </span>
+        <span v-if="outcome.raw_rewards?.filter(f=>f.reward_type==6)?.length > 0"
+      class="inline-flex items-center mr-1.5 whitespace-nowrap">
+    <img class="h-6 w-6 -m-1 mr-0 p-1"
+         :src="iconURL('egginc-extras/icon_piggy_golden_egg.png', 64)" />
+    <span class="-ml-1">{{ formatFloat(outcome.raw_rewards?.filter(f=>f.reward_type==6)[0].reward_amount, { digits: 2 }) }}</span>
+</span>
       <span
         v-for="byproduct in outcome.product_rewards"
         :key="byproduct.id"
