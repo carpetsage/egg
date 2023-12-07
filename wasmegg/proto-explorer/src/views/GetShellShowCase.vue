2b6e7b554af7dd1316346b1124bcf83ff3e99d47
TiaBroch <tiabroch@gmail.com>
2023-12-07 11:22:08 +0100

    Added proto section for the Shell Showcase

---
+++ wasmegg/proto-explorer/src/views/GetShellShowCase.vue
@@ -0,0 +1,38 @@
+<template>
+    <api-requester api-endpoint="/ei/get_shell_showcase"
+                   request-message="BasicRequestInfo"
+                   response-message="ShellShowcase"
+                   :response-authenticated="true"
+                   :persist-form-data="() => {}"
+                   :get-request-payload-object="getRequestPayloadObject">
+        <template #form-body>
+            <request-button :form-valid="true" />
+        </template>
+    </api-requester>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    import { CLIENT_VERSION, ei } from 'lib';
+    import ApiRequester from '@/components/APIRequester.vue';
+    import RequestButton from '@/components/RequestButton.vue';
+
+    type ArtifactsConfigurationRequestPayload = Omit<ei.IArtifactsConfigurationRequest, ''>;
+
+    export default defineComponent({
+        components: {
+            ApiRequester,
+            RequestButton,
+        },
+        setup() {
+            const getRequestPayloadObject = (): ArtifactsConfigurationRequestPayload => ({
+                clientVersion: CLIENT_VERSION,
+            });
+            return {
+                getRequestPayloadObject,
+            };
+        },
+    });
+</script>
+
