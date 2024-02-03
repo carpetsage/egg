f724224a0112290c851d4402fa7931cfd69d1a34
Carpet Sage <carpetsage@protonmail.com>
2024-02-03 16:32:07 -0600

    new icon paths

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,7 @@
-export function getLocalStorage(key: string, prefix?: string): string | undefined {
+export function getLocalStorage(
+  key: string,
+  prefix?: string,
+): string | undefined {
   if (prefix === undefined) {
     prefix = `${window.location.pathname}_`;
   }
@@ -11,10 +14,14 @@ export function getLocalStorage(key: string, prefix?: string): string | undefine
 }
 
 export function getLocalStorageNoPrefix(key: string): string | undefined {
-  return getLocalStorage(key, '');
+  return getLocalStorage(key, "");
 }
 
-export function setLocalStorage(key: string, val: unknown, prefix?: string): void {
+export function setLocalStorage(
+  key: string,
+  val: unknown,
+  prefix?: string,
+): void {
   if (prefix === undefined) {
     prefix = `${window.location.pathname}_`;
   }
@@ -26,36 +33,37 @@ export function setLocalStorage(key: string, val: unknown, prefix?: string): voi
 }
 
 export function setLocalStorageNoPrefix(key: string, val: unknown): void {
-  setLocalStorage(key, val, '');
+  setLocalStorage(key, val, "");
 }
 
 export function iconURL(
   relpath: string,
-  size: number | string = 'orig',
-  extension?: 'png' | 'svg' | 'webp'
+  size: number | string = "orig",
+  extension?: "png" | "svg" | "webp",
 ): string {
   if (extension) {
     relpath = relpath.replace(/\.[a-z]+$/, `.${extension}`);
   }
   if (/icon_dilithium_bulb/.test(relpath)) {
-    return 'https://imagedelivery.net/3eHDWErDPIKAYL6ofL-Kwg/f9be3c0a-0bac-483f-b0b2-143356ccd800/public';
-  }
-  else if (/sub_icon/.test(relpath)) {
-    return 'https://imagedelivery.net/3eHDWErDPIKAYL6ofL-Kwg/3b23e937-69ad-42e4-0034-841d6d494600/public';
+    return `https://imagedelivery.net/3eHDWErDPIKAYL6ofL-Kwg/${size}/egginc/b_icon_dilithium_bulb.png/public`;
+  } else if (/sub_icon/.test(relpath)) {
+    return `https://imagedelivery.net/3eHDWErDPIKAYL6ofL-Kwg/${size}/egginc/sub_icon.png/public`;
   }
   return `https://eggincassets.tcl.sh/${size}/${relpath}`;
 }
 
 // Trim trailing zeros, and possibly the decimal point.
 export function trimTrailingZeros(s: string): string {
-  s = s.replace(/0+$/, '');
-  if (s.endsWith('.')) {
+  s = s.replace(/0+$/, "");
+  if (s.endsWith(".")) {
     s = s.substring(0, s.length - 1);
   }
   return s;
 }
 
 export function titleCase(s: string): string {
-  const title = s.toLowerCase().split(/_|-| /).map(x => x.length > 3 ? x[0].toUpperCase() + x.substring(1) : x).join(' ');
+  const title = s.toLowerCase().split(/_|-| /).map((x) =>
+    x.length > 3 ? x[0].toUpperCase() + x.substring(1) : x
+  ).join(" ");
   return title[0].toUpperCase() + title.substring(1);
 }
