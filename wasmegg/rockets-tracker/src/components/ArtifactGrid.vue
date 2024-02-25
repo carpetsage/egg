3157c7523c8dd075917185ec19cef477974bc314
Carpet Sage <carpetsage@protonmail.com>
2024-02-25 15:14:04 -0600

    minor changes. Remove weird tab characters. Simplify ArtifactGrid craft chance display templating with loops. Add odds_multiplier to lib/artifacts/data.json.d.ts

--- wasmegg/rockets-tracker/src/components/ArtifactGrid.vue
+++ wasmegg/rockets-tracker/src/components/ArtifactGrid.vue
@@ -138,37 +138,25 @@
                           You spent an estimated
                           <span class="text-yellow-300">{{ ts(tier.sunkCost) }}</span>
                           golden eggs on crafting this item.
-                          
-                          <template v-if="tier.isArtifact == true">
-                            The next craft has a 
-                            {{ts(nextCraftRarityChance(tier,craftingLevel.rarityMult,0,tier.crafted))}}%
-                            <template v-if="tier.isArtifact == true && tier.possibleRarity(1) == true">
-                              /
-                              <span class="text-blue-300">{{ts(nextCraftRarityChance(tier,craftingLevel.rarityMult,1,tier.crafted))}}%</span>
+
+                          <template v-if="tier.isArtifact">
+                            The next craft has a
+                            <template v-for="rarity in tier.possibleRarities" :key="rarity">
+                              <template v-if="rarity != Rarity.COMMON"> / </template>
+                              <span :class="artifactRarityFgClassBright(rarity)">
+                                {{ ts(nextCraftRarityChance(tier, craftingLevel.rarityMult, rarity, tier.crafted)) }}%
+                              </span>
                             </template>
-                            <template v-if="tier.isArtifact == true && tier.possibleRarity(2) == true">
-                              /
-                              <span class="text-purple-300">{{ts(nextCraftRarityChance(tier,craftingLevel.rarityMult,2,tier.crafted))}}%</span>
+                            chance of being
+                            <template v-for="rarity in tier.possibleRarities" :key="rarity">
+                              <template v-if="rarity != Rarity.COMMON"> / </template>
+                              <span :class="artifactRarityFgClassBright(rarity)">
+                                {{ titleCase(Rarity[rarity]) }}
+                              </span>
                             </template>
-                            <template v-if="tier.isArtifact == true && tier.possibleRarity(3) == true">
-                              /
-                              <span class="text-yellow-300">{{ts(nextCraftRarityChance(tier,craftingLevel.rarityMult,3,tier.crafted))}}%</span>
-                            </template>
-                            chance of being Common
-                            <template v-if="tier.isArtifact == true && tier.possibleRarity(1) == true">
-                              /
-                              <span class="text-blue-300"> Rare </span>
-                            </template>
-                            <template v-if="tier.isArtifact == true && tier.possibleRarity(2) == true">
-                              /
-                              <span class="text-purple-300"> Epic </span>
-                            </template>
-                            <template v-if="tier.isArtifact == true && tier.possibleRarity(3) == true">
-                              /
-                              <span class="text-yellow-300"> Legendary</span>
-                            </template>.
+                            .
                           </template>
-                          
+
                           <template v-if="craftableCount(tier) === 0"
                             >The next craft is going to cost
                             <span class="text-blue-300">{{ ts(tier.nextCraftCost) }}</span>
@@ -219,19 +207,13 @@
 <script lang="ts">
 import { computed, defineComponent, PropType, toRefs } from 'vue';
 import { Tippy } from 'vue-tippy';
-
-import { ArtifactSet, ei, iconURL, Inventory, InventoryFamily, InventoryItem,  getCraftingLevelFromXp,
-  PlayerCraftingLevel,
-  getXPFromCraftingLevel } from 'lib';
-import { artifactRarityFgClass } from '@/utils';
+import { ArtifactSet, ei, iconURL, Inventory, InventoryFamily, InventoryItem, getCraftingLevelFromXp, titleCase } from 'lib';
+import { artifactRarityFgClass, artifactRarityFgClassBright } from '@/utils';
 import ArtifactRecipe from '@/components/ArtifactRecipe.vue';
 import ArtifactGallery from './ArtifactGallery.vue';
 
 type ItemId = string;
 const TOOMUCHSHIT = 9000000;
-
-
-
 
 export default defineComponent({
   components: {
@@ -311,25 +293,27 @@ export default defineComponent({
     };
     const nextCraftRarityChance = (item : InventoryItem, craft_multiplier : number, rarity : number, crafted_count : number) => {
       let craft_chance = 100.0; //For Common: Initialize with 100% chance
-      if (rarity > 0) {	
-      	craft_chance = item.craftChance(craft_multiplier,rarity,crafted_count); //Rare or better
+      if (rarity > 0) {
+        craft_chance = item.craftChance(craft_multiplier,rarity,crafted_count); //Rare or better
       }
-      for (let i = rarity+1; i < 4; i++){
-      	if (item.possibleRarity(i) == true) {
-          craft_chance = craft_chance - item.craftChance(craft_multiplier,i,crafted_count); //Subtract resulting chance of next higher existing rarity and return. Look at Auxbrain's medium post about craft chances.
-      	  return craft_chance;
-      	}
+      for (let i = rarity+1; i < 4; i++) {
+        if (item.possibleRarity(i)) {
+          //Subtract resulting chance of next higher existing rarity and return. Look at Auxbrain's medium post about craft chances.
+          craft_chance = craft_chance - item.craftChance(craft_multiplier,i,crafted_count);
+          return craft_chance;
+        }
       }
       return craft_chance;
     }
-    
+
     const craftingXp = computed(() => Math.floor(backup.value.artifacts?.craftingXp || 0));
     const craftingLevel = computed(() => getCraftingLevelFromXp(craftingXp.value));
-    
+
     return {
       artifactExplorerLink,
       Rarity: ei.ArtifactSpec.Rarity,
       artifactRarityFgClass,
+      artifactRarityFgClassBright,
       craftableCount,
       nextRecursiveCraftable,
       nextRecursiveCraftCost,
@@ -338,6 +322,7 @@ export default defineComponent({
       craftingLevel,
       iconURL,
       ts,
+      titleCase,
     };
   },
 });
