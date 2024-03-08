a6017b31bee3c3e2fb08aa4b045fcd2a6753830c
Carpet Sage <carpetsage@protonmail.com>
2024-03-08 17:34:06 -0600

    switch to new loot data only

--- wasmegg/artifact-explorer/update-loot-data.sh
+++ wasmegg/artifact-explorer/update-loot-data.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 echo "Downloading loot.json"
-FULLDATA_URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetData"
-TRIMMEDATA_URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed"
-URL=$TRIMMEDATA_URL
-if [[ $1 == "test" ]]; then
-  URL='https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed?newDropsOnly'
-fi
-HTTP_CODE=$(curl -so loot_raw.json --write-out "%{http_code}" ${URL})
-echo "HTTP $HTTP_CODE"
+#URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetData"
+#URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed"
+#URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed?oldDropsOnly"
+URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed?newDropsOnly"
+HTTP_CODE=$(curl -so loot_raw.json --write-out "%{http_code}" "${URL}")
+echo "HTTP ${HTTP_CODE}"
 if [ -s loot_raw.json ]; then
   echo 'compacting json'
-  jq -c . loot_raw.json > $(dirname $0)/src/lib/loot.json
+  jq -c . loot_raw.json > "$(dirname $0)"/src/lib/loot.json
 fi
 rm loot_raw.json
