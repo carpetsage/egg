77774f12845f6a6fae29d60651a19f0495ee139b
Carpet Sage <carpetsage@protonmail.com>
2024-03-08 17:38:37 -0600

    display old loot data

--- wasmegg/artifact-explorer/update-loot-data.sh
+++ wasmegg/artifact-explorer/update-loot-data.sh
@@ -2,8 +2,8 @@
 echo "Downloading loot.json"
 #URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetData"
 #URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed"
-#URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed?oldDropsOnly"
-URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed?newDropsOnly"
+URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed?oldDropsOnly"
+#URL="https://eggincdatacollection.azurewebsites.net/api/GetCarpetDataTrimmed?newDropsOnly"
 HTTP_CODE=$(curl -so loot_raw.json --write-out "%{http_code}" "${URL}")
 echo "HTTP ${HTTP_CODE}"
 if [ -s loot_raw.json ]; then
