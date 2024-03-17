e1fffa192572cd947b5b014cac978868314fbc35
Carpet Sage <carpetsage@protonmail.com>
2024-03-16 21:55:12 -0500

    remove vite rollup compression stuff

--- eicoop/vite.config.ts
+++ eicoop/vite.config.ts
@@ -1,13 +1,9 @@
-import { brotliCompress } from 'zlib';
-import { promisify } from 'util';
 import path from 'path';
 import process from 'process';
 import { splitVendorChunkPlugin, ConfigEnv, UserConfigExport } from 'vite';
-import gzip from 'rollup-plugin-gzip';
 import vue from '@vitejs/plugin-vue';
 import { viteMockServe } from 'vite-plugin-mock';
 
-const brotliPromise = promisify(brotliCompress)
 // Run VITE_APP_MOCK=1 yarn dev to enable API mocking.
 
 // https://vitejs.dev/config/
@@ -25,8 +21,6 @@ export default ({ command }: ConfigEnv): UserConfigExport => ({
       localEnabled: command === 'serve' && !!process.env.VITE_APP_MOCK,
     }),
     splitVendorChunkPlugin(),
-    gzip(),
-    gzip({customCompression: c => brotliPromise(Buffer.from(c)), fileName: '.br'}),
   ],
   build: {
     chunkSizeWarningLimit: 3000,
