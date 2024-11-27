d0b62e4ec0b2b39cde8e4ac74dac4f64c9f521ad
carpetsage <carpetsage@protonmail.com>
2024-11-27 13:11:01 -0600

    only push new config gist if config is non-empty

--- update-egg-config-gist/update-config.sh
+++ update-egg-config-gist/update-config.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 ./getConfig.py > config.json
 # delete isNew and secondsRemaining fields if they exist
 jq -S 'walk(try (del(.secondsRemaining) | del(.secondsUntilAvailable))//.)' config.json > configstripped.json
 wget https://gist.githubusercontent.com/carpetsage/${GIST_ID}/raw/config.json -O /tmp/gist.json
 jq -S 'walk(try (del(.secondsRemaining) | del(.secondsUntilAvailable))//.)' /tmp/gist.json > giststripped.json
-# push gist if there's a change
-diff configstripped.json giststripped.json || node ./push-gist.js
+# push gist if there's a change and config.json not empty
+if [ -s config.json ]; then
+  diff configstripped.json giststripped.json || node ./push-gist.js
+fi
