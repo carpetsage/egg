59f14eed737f08d3b52fe4ce2d93601e51973bce
Carpet Sage <carpetsage@protonmail.com>
2025-01-27 21:07:11 -0600

    update proto

--- lib/api/version.ts
+++ lib/api/version.ts
@@ -1,7 +1,7 @@
 import { ei } from '../proto';
 
-export const APP_VERSION = '1.33';
-export const APP_BUILD = '111290';
-export const CLIENT_VERSION = 67;
+export const APP_VERSION = '1.34.1';
+export const APP_BUILD = '111300';
+export const CLIENT_VERSION = 68;
 export const PLATFORM_STRING = 'IOS';
 export const PLATFORM = ei.Platform.IOS;
