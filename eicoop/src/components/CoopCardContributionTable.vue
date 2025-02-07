9858ceb20400c244051b42ee574e5b9dff1842fe
Carpet Sage <carpetsage@protonmail.com>
2025-02-07 17:52:02 -0600

    add indicator for autojoiners in private coops

--- eicoop/src/components/CoopCardContributionTable.vue
+++ eicoop/src/components/CoopCardContributionTable.vue
@@ -69,16 +69,23 @@
         >
           <template v-if="devmode">
             <base-click-to-copy
-              :text="contributor.id"
+              :text="contributor.name"
               class="text-gray-500 dark:text-gray-200 truncate"
             >
               {{ renderNonempty(contributor.name) }}
-              <template #tooltip>Click to copy ID: {{ contributor.id }}</template>
+              <template #tooltip>Click to copy name: {{ contributor.name }}</template>
             </base-click-to-copy>
           </template>
           <template v-else>
             <span class="text-gray-500 dark:text-gray-200 truncate">{{ contributor.name }}</span>
           </template>
+          <svg v-if="!coopStatus.isPublic && contributor.autojoined" v-tippy="{ content: 'Autojoiner in Private Coop' }"
+            class="h-4 w-4 text-gray-400 dark:text-gray-300 ml-1.5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+              <path fill-rule="evenodd"
+                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                clip-rule="evenodd" />
+          </svg>
           <!-- Inactive -->
           <svg
             v-if="!contributor.isActive"
