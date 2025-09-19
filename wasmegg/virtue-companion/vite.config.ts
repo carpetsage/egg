83b0699dd3e0aea35251d4733ab48c5bda3306c1
Carpet Sage <carpetsage@protonmail.com>
2025-09-18 21:06:55 -0500

    fix name

--- wasmegg/virtue-companion/vite.config.ts
+++ wasmegg/virtue-companion/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/virtue/',
+  base: '/virtue-companion/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
