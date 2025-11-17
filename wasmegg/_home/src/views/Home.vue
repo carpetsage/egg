554859e9cf8063c47229b428710a7a82bfeb1b8d
Carpet Sage <carpetsage@protonmail.com>
2025-11-16 20:50:00 -0600

    add virtue companion to home page

--- wasmegg/_home/src/views/Home.vue
+++ wasmegg/_home/src/views/Home.vue
@@ -6,11 +6,7 @@
 
   <div v-if="daysVisitedStreak >= EASTER_EGG_DAY" class="my-2 text-center">
     <div class="w-max mx-auto bg-yellow-500 text-sm text-white px-2 py-1 rounded-md">
-      <svg
-        class="inline h-4 w-4 text-red-500 relative -top-px -left-px"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
+      <svg class="inline h-4 w-4 text-red-500 relative -top-px -left-px" viewBox="0 0 20 20" fill="currentColor">
         <path
           fill-rule="evenodd"
           d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
@@ -29,8 +25,8 @@
 
   <p>
     I am <span class="font-medium">carpetsage</span> on the
-    <base-link href="https://discord.gg/egginc">Egg, Inc. Discord server</base-link>. This is the
-    index of my fork of <span class="font-medium">mk2#4590's</span> public tools for Egg, Inc.
+    <base-link href="https://discord.gg/egginc">Egg, Inc. Discord server</base-link>. This is the index of my fork of
+    <span class="font-medium">mk2#4590's</span> public tools for Egg, Inc.
   </p>
 
   <whats-new class="my-4" />
@@ -46,6 +42,7 @@
     <li><tool-description id="artifact-explorer" @visit="onvisit" /></li>
     <li><tool-description id="artifact-sandbox" @visit="onvisit" /></li>
     <li><tool-description id="rockets-tracker" @visit="onvisit" /></li>
+    <li><tool-description id="virtue-companion" @visit="onvisit" /></li>
     <li><tool-description id="past-contracts" @visit="onvisit" /></li>
     <li><tool-description id="inventory-visualizer" @visit="onvisit" /></li>
     <li><tool-description id="enlightenment" @visit="onvisit" /></li>
@@ -68,8 +65,8 @@
     <li><tool-description id="researches" @visit="onvisit" /></li>
   </ul>
   <p>
-    Certain tools are not advertised here due to concerns. If you are interested, poke around the
-    source repo to find them.
+    Certain tools are not advertised here due to concerns. If you are interested, poke around the source repo to find
+    them.
   </p>
 
   <h2>Miscellaneous small tools</h2>
@@ -96,8 +93,8 @@
         >
         <sup class="text-xs font-medium uppercase text-green-600 ml-1">Secret</sup>
       </span>
-      &mdash; Strategies so next-level, they will blow your effing mind. You're advised to stay
-      away. Not for the faint-hearted.
+      &mdash; Strategies so next-level, they will blow your effing mind. You're advised to stay away. Not for the
+      faint-hearted.
     </li>
   </ul>
 
@@ -111,9 +108,7 @@
       >
         Donate to help cover hosting costs
         <span v-if="!donationPageVisited" class="absolute top-0.5 -right-3 flex h-2 w-2">
-          <span
-            class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"
-          ></span>
+          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
           <span class="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
         </span>
       </base-router-link>
@@ -127,8 +122,8 @@
 
   <h2>Supporters</h2>
   <p>
-    Hosting costs come from donations of the awesome individuals listed below. Find out how you can
-    support my tools by visiting the
+    Hosting costs come from donations of the awesome individuals listed below. Find out how you can support my tools by
+    visiting the
     <base-router-link
       :to="{ name: 'donate' }"
       class="relative text-green-500 hover:text-green-600 visited:text-green-500 hover:visited:text-green-600"
@@ -186,8 +181,7 @@ export default defineComponent({
     };
     const triggerEasterEggAndDisableInTheFuture = () => {
       triggerEasterEgg();
-      const easterEggShownForTheFirstTime =
-        getLocalStorageNoPrefix(EASTER_EGG_SHOWN_AT_LOCALSTORAGE_KEY) === undefined;
+      const easterEggShownForTheFirstTime = getLocalStorageNoPrefix(EASTER_EGG_SHOWN_AT_LOCALSTORAGE_KEY) === undefined;
       setLocalStorageNoPrefix(EASTER_EGG_SHOWN_AT_LOCALSTORAGE_KEY, Date.now());
       if (easterEggShownForTheFirstTime) {
         goatcounter?.count({
