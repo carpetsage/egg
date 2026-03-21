a5a45dc5eece6088aa846fe014c7d76eaf167d57
joobrainie <joobrainie@gmail.com>
2026-03-21 22:49:02 +0200

    clarify shift reconciliation text

--- wasmegg/ascension-planner/src/components/ActionGroup.vue
+++ wasmegg/ascension-planner/src/components/ActionGroup.vue
@@ -66,12 +66,12 @@
 
         <!-- Reconciliation Status for the whole shift -->
         <div v-if="actionsStore.isReconciling" class="shrink-0">
-          <div v-if="shiftReconciliationStatus === 'completed'" class="text-emerald-500" v-tippy="'Shift completed in-game'">
+          <div v-if="shiftReconciliationStatus === 'completed'" class="text-emerald-500" v-tippy="'All actions complete.'">
             <svg class="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 20 20">
               <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
             </svg>
           </div>
-          <div v-else-if="shiftReconciliationStatus === 'pending'" class="text-amber-500 animate-pulse" v-tippy="'Shift pending in-game'">
+          <div v-else-if="shiftReconciliationStatus === 'pending'" class="text-amber-500 animate-pulse" v-tippy="'Some actions pending.'">
             <svg class="w-5 h-5 sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
